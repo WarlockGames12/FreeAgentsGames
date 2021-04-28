dde9ff428005fa8602e39d2338c03fa5f29815a4
WarlockGames12 <fpweeyou@gmail.com>
2021-04-28 11:21:37 +0200

    Main Menu

---
+++ The Beevasion/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void NewGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void Options()
+    {
+
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("This thing works if you wanna know");
+        Application.Quit();
+    }
+}

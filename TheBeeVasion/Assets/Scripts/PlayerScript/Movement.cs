8526c6fa8f4310ddd050e00e17ffaed513ac840a
WarlockGames12 <fpweeyou@gmail.com>
2021-04-30 11:24:28 +0200

    new version

---
+++ TheBeeVasion/Assets/Scripts/PlayerScript/Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float movementSpeed = 3;
+    
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-movementSpeed, 0, 0) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(movementSpeed, 0, 0) * Time.deltaTime;
+        }
+    }
+}

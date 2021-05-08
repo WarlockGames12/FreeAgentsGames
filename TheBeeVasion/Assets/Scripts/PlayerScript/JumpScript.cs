46c7a46f9dc540ffb6b6231fac9753987392a14e
WarlockGames12 <fpweeyou@gmail.com>
2021-05-08 21:23:35 +0200

    Fixes to some things

---
+++ TheBeeVasion/Assets/Scripts/PlayerScript/JumpScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpScript : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float jumpPower = 8;
+    public bool canJump = true;
+    public AudioSource JumpPlayer;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && canJump == true)
+        {
+
+            JumpPlayer.Play();
+            rb.velocity = new Vector3(0, jumpPower, 0);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        canJump = true;
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        canJump = false;
+    }
+}
